820b4def26dc33e73bbc9919fff3cd0a02467d19
Rodney Lab Codesign <codesign@rodneylab.com>
2021-05-01 19:10:58 +0100

    feat(main): 🖥  added newton-raphson solution of opacity for given target contrast ratio

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,57 @@ fn main() {
     let contrast = contrast_ratio(&mixed, &Rgb::new(255, 255, 255));
     println!("Overlay contrast ratio: {}", contrast);
 
-    read_in_colour();
+    // read_in_colour();
+
+    let lightest_rgb = lowest_highest_luminance_rgb(&resized_image).1;
+    println!("lightest background: {:?}", lightest_rgb);
+    let alpha = overlay_opacity(
+        &Rgb::new(255, 255, 255),
+        &lightest_rgb,
+        &Rgb::new(0, 0, 0),
+        4.5,
+    );
+    println!("Overlay alpha: {}", alpha);
+
+    // check
+    let new_lightest = composite_colour(&lightest_rgb, &Rgb::new(0, 0, 0), &alpha);
+    println!("new lightest: {:?}", new_lightest);
+    let actual_contrast_ratio = contrast_ratio(&new_lightest, &Rgb::new(255, 255, 255));
+    println!("Actual contrast ratio: {}", actual_contrast_ratio);
+}
+
+fn overlay_opacity(
+    foreground_colour: &Rgb,
+    background_colour: &Rgb,
+    overlay_colour: &Rgb,
+    contrast_ratio: f64,
+) -> f64 {
+    let foreground_luminance = relative_luminance(foreground_colour);
+    let background_luminance = relative_luminance(background_colour);
+    let background_colour_ratio = rgb_ratio(background_colour);
+    let overlay_colour_ratio = rgb_ratio(overlay_colour);
+
+    let composite_luminance = ((foreground_luminance + 0.05) / contrast_ratio) - 0.05;
+    println!("Composite luminance: {}", composite_luminance);
+    let delta = delta_from_colour_target_luminance(&background_colour_ratio, composite_luminance);
+
+    let composite_colour_ratio = RgbRatio::new(
+        background_colour_ratio.get_red() + delta,
+        background_colour_ratio.get_green() + delta,
+        background_colour_ratio.get_blue() + delta,
+    );
+    let opacity_r = (background_colour_ratio.get_red() - composite_colour_ratio.get_red())
+        / (background_colour_ratio.get_red() + overlay_colour_ratio.get_red());
+    let opacity_g = (background_colour_ratio.get_green() - composite_colour_ratio.get_green())
+        / (background_colour_ratio.get_green() + overlay_colour_ratio.get_green());
+    let opacity_b = (background_colour_ratio.get_blue() - composite_colour_ratio.get_blue())
+        / (background_colour_ratio.get_blue() + overlay_colour_ratio.get_blue());
+    println!("Foreground luminance: {}", foreground_luminance);
+    println!("Background luminance: {}", background_luminance);
+    println!("Composite luminance: {}", composite_luminance);
+    println!("Delta: {}", delta);
+    println!("Overlay alpha: {} {} {}", opacity_r, opacity_g, opacity_b);
+    (opacity_r + opacity_g + opacity_b) / 3.0
 }
 
 fn composite_colour(base_colour: &Rgb, overlay_colour: &Rgb, overlay_opacity: &f64) -> Rgb {
@@ -39,6 +89,7 @@ fn composite_colour(base_colour: &Rgb, overlay_colour: &Rgb, overlay_opacity: &f
     let b = base_colour.get_blue() as f64 * (1.0 - overlay_opacity)
         + overlay_colour.get_blue() as f64 * overlay_opacity;
     Rgb::new(r.trunc() as u8, g.trunc() as u8, b.trunc() as u8)
+    // Rgb::new(r.ceil() as u8, g.ceil() as u8, b.ceil() as u8)
 }
 
 fn contrast_ratio_from_relative_luminance(
@@ -59,6 +110,52 @@ fn contrast_ratio(colour_1: &Rgb, colour_2: &Rgb) -> f64 {
     )
 }
 
+// Newton-Raphson solution of delta
+fn delta_from_colour_target_luminance(colour_ratio: &RgbRatio, target_luminance: f64) -> f64 {
+    let mut delta_current = -0.1;
+    let initial_red_ratio = colour_ratio.get_red();
+    let initial_green_ratio = colour_ratio.get_green();
+    let initial_blue_ratio = colour_ratio.get_blue();
+    let mut colour_ratio_current = RgbRatio::new(
+        initial_red_ratio + delta_current,
+        initial_green_ratio + delta_current,
+        initial_blue_ratio + delta_current,
+    );
+    let mut delta_next = delta_current
+        - (relative_luminance_from_colour_ratio(&colour_ratio_current)
+            / relative_luminance_derivative(&colour_ratio_current));
+    let mut colour_ratio_next = RgbRatio::new(
+        initial_red_ratio + delta_next,
+        initial_green_ratio + delta_next,
+        initial_blue_ratio + delta_next,
+    );
+    let mut luminance_current = relative_luminance_from_colour_ratio(&colour_ratio_next);
+
+    let eps = 1.0e-6;
+    let mut iteration = 1;
+
+    while (target_luminance - luminance_current).abs() > eps {
+        // while iteration < 11 {
+        println!(
+            "Iteration: {} {} {} {}",
+            iteration, delta_current, delta_next, luminance_current
+        );
+        delta_current = delta_next;
+        colour_ratio_current = colour_ratio_next;
+        delta_next = delta_current
+            - ((relative_luminance_from_colour_ratio(&colour_ratio_current) - target_luminance)
+                / relative_luminance_derivative(&colour_ratio_current));
+        colour_ratio_next = RgbRatio::new(
+            initial_red_ratio + delta_next,
+            initial_green_ratio + delta_next,
+            initial_blue_ratio + delta_next,
+        );
+        luminance_current = relative_luminance_from_colour_ratio(&colour_ratio_next);
+        iteration = iteration + 1;
+    }
+    delta_next
+}
+
 fn lowest_highest_luminance(image: &PhotonImage) -> (f64, f64) {
     let mut highest_luminance = 0.0;
     let mut lowest_luminance = 1.0;
@@ -73,6 +170,26 @@ fn lowest_highest_luminance(image: &PhotonImage) -> (f64, f64) {
         }
     }
     (lowest_luminance, highest_luminance)
+}
+
+fn lowest_highest_luminance_rgb(image: &PhotonImage) -> (Rgb, Rgb) {
+    let mut highest_luminance = 0.0;
+    let mut lowest_luminance = 1.0;
+    let mut highest_luminance_rgb = Rgb::new(0, 0, 0);
+    let mut lowest_luminance_rgb = Rgb::new(255, 255, 255);
+    let raw_pixels = image.get_raw_pixels();
+    for pixel in raw_pixels.chunks(4) {
+        let pixel_rgb = Rgb::new(pixel[0], pixel[1], pixel[2]);
+        let pixel_luminance = relative_luminance(&pixel_rgb);
+        if pixel_luminance > highest_luminance {
+            highest_luminance = pixel_luminance;
+            highest_luminance_rgb = pixel_rgb;
+        } else if pixel_luminance < lowest_luminance {
+            lowest_luminance = pixel_luminance;
+            lowest_luminance_rgb = pixel_rgb;
+        }
+    }
+    (lowest_luminance_rgb, highest_luminance_rgb)
 }
 
 fn read_in_colour() -> photon_rs::Rgb {
@@ -154,23 +271,45 @@ fn rgb_ratio(colour: &Rgb) -> RgbRatio {
 
 fn relative_luminance(colour: &Rgb) -> f64 {
     let standard_rgb_colour = rgb_ratio(colour);
+    relative_luminance_from_colour_ratio(&standard_rgb_colour)
+}
 
-    let linear_r = if standard_rgb_colour.get_red() <= 0.03928 {
-        standard_rgb_colour.get_red() / 12.92
+fn relative_luminance_from_colour_ratio(colour_ratio: &RgbRatio) -> f64 {
+    let linear_r = if colour_ratio.get_red() <= 0.03928 {
+        colour_ratio.get_red() / 12.92
     } else {
-        ((standard_rgb_colour.get_red() + 0.055) / 1.055).powf(2.4)
+        ((colour_ratio.get_red() + 0.055) / 1.055).powf(2.4)
     };
-    let linear_g = if standard_rgb_colour.get_green() <= 0.03928 {
-        standard_rgb_colour.get_green() / 12.92
+    let linear_g = if colour_ratio.get_green() <= 0.03928 {
+        colour_ratio.get_green() / 12.92
     } else {
-        ((standard_rgb_colour.get_green() + 0.055) / 1.055).powf(2.4)
+        ((colour_ratio.get_green() + 0.055) / 1.055).powf(2.4)
     };
-    let linear_b = if standard_rgb_colour.get_blue() <= 0.03928 {
-        standard_rgb_colour.get_blue() / 12.92
+    let linear_b = if colour_ratio.get_blue() <= 0.03928 {
+        colour_ratio.get_blue() / 12.92
     } else {
-        ((standard_rgb_colour.get_blue() + 0.055) / 1.055).powf(2.4)
+        ((colour_ratio.get_blue() + 0.055) / 1.055).powf(2.4)
     };
     0.2126 * linear_r + 0.7152 * linear_g + 0.0722 * linear_b
+}
+
+fn relative_luminance_derivative(colour_ratio: &RgbRatio) -> f64 {
+    let linear_r = if colour_ratio.get_red() <= 0.03928 {
+        1.0 / 12.92
+    } else {
+        ((colour_ratio.get_red() + 0.055) / 1.055).powf(1.4)
+    };
+    let linear_g = if colour_ratio.get_green() <= 0.03928 {
+        1.0 / 12.92
+    } else {
+        ((colour_ratio.get_green() + 0.055) / 1.055).powf(1.4)
+    };
+    let linear_b = if colour_ratio.get_blue() <= 0.03928 {
+        1.0 / 12.92
+    } else {
+        ((colour_ratio.get_blue() + 0.055) / 1.055).powf(1.4)
+    };
+    (2.4 / 1.055) * (0.2126 * linear_r + 0.7152 * linear_g + 0.0722 * linear_b)
 }
 
 #[cfg(test)]
