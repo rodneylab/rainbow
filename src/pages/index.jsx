dc98ba133a0dc98d7c756794b15d10b54ecfed58
Rodney Lab Codesign <codesign@rodneylab.com>
2021-05-08 10:11:42 +0100

    feat: 📸 added rudimentary image upload and preview

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
@@ -7,6 +7,9 @@ import { PureLayout as Layout } from '../components/Layout';
 import { PureSEO as SEO } from '../components/SEO';
 
 export default function Home({ data }) {
+  const [files, setFiles] = useState([]);
+  const [imagePreviewURL, setImagePreviewURL] = useState();
+
   const handleClick = async () => {
     try {
       const response = await axios({
@@ -26,15 +29,48 @@ export default function Home({ data }) {
     }
   };
 
+  const handleFileInput = (event) => {
+    console.log(event.target);
+    setFiles(event.target.files);
+    console.log('file: ', files[0]);
+    let reader = new FileReader();
+    reader.onloadend = () => {
+      setImagePreviewURL(reader.result);
+    };
+    reader.readAsDataURL(files[0]);
+  };
+
   return (
     <>
-      <SEO data={data} title="Home" metadescription="Climate - Gatsby v3 Starter for MDX Gatsby Blog" />
+      <SEO
+        data={data}
+        title="Home"
+        metadescription="Climate - Gatsby v3 Starter for MDX Gatsby Blog"
+      />
       <Layout data={data}>
         <>
           <header>
-            <h1>Rainbow</h1>
+            <h1>Rainbow Contrast Checker</h1>
           </header>
-          <button type="submit" onClick={() => { handleClick(); }}>Invoke Serverless</button>
+          <form method="post" encType="multipart/form-data">
+            <div>
+              <label htmlFor="file">Choose file to upload</label>
+              <input onChange={handleFileInput} type="file" id="file" accept="image/*" />
+            </div>
+            {/* <div>
+              <button type="submit">Submit</button>
+            </div> */}
+          </form>
+          <br />
+          <img alt="user uploaded content" id="myImg" src={imagePreviewURL} />
+          <button
+            type="submit"
+            onClick={() => {
+              handleClick();
+            }}
+          >
+            Invoke Serverless
+          </button>
         </>
       </Layout>
     </>
