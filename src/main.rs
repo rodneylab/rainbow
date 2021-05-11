79e8282daa56b22d63fbc4170f4510fca9300c58
Rodney Lab Codesign <codesign@rodneylab.com>
2021-05-11 07:17:47 +0100

    feat: 📸 added functionality to read base64 image and return alpha

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,36 @@
 use netlify_lambda_http::{handler, lambda::Context, IntoResponse, Request, RequestExt};
 extern crate photon_rs;
-use photon_rs::native::open_image;
 use photon_rs::transform::resize;
-use photon_rs::PhotonImage;
-use photon_rs::Rgb;
+use photon_rs::{base64_to_image, PhotonImage, Rgb};
 use serde_json::json;
-use std::io;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // netlify_lambda::run(handler(|_request, _context| async {
-    //     Ok("🦀 Hello, Netlify 🦀")
-    // }))
-    // .await?;
-
-    // let resized_image;
-    if let Ok(img) = open_image("image.jpg") {
-        let resized_image = resize_image(&img);
-        let min_max_luminance = lowest_highest_luminance(&resized_image);
-        println!("Lowest luminance: {}", min_max_luminance.0);
-        println!("Highest luminance: {}", min_max_luminance.1);
-
-        let white_contrast_ratio = contrast_ratio_from_relative_luminance(
-            &relative_luminance(&Rgb::new(255, 255, 255)),
-            &min_max_luminance.1,
-        );
-        let black_contrast_ratio = contrast_ratio_from_relative_luminance(
-            &relative_luminance(&Rgb::new(0, 0, 0)),
-            &min_max_luminance.0,
-        );
-        println!("White luminance: {}", white_contrast_ratio);
-        println!("Black luminance: {}", black_contrast_ratio);
-
-        let mixed = composite_colour(&Rgb::new(0, 0, 0), &Rgb::new(255, 255, 255), &1.0);
-        let contrast = contrast_ratio(&mixed, &Rgb::new(255, 255, 255));
-        println!("Overlay contrast ratio: {}", contrast);
-
-        // read_in_colour();
-
-        let lightest_rgb = lowest_highest_luminance_rgb(&resized_image).1;
-        println!("lightest background: {:?}", lightest_rgb);
-        let alpha = overlay_opacity(
-            &Rgb::new(255, 255, 255),
-            &lightest_rgb,
-            &Rgb::new(0, 0, 0),
-            4.5,
-        );
-        println!("Overlay alpha: {}", alpha);
-
-        // check
-        let new_lightest = composite_colour(&lightest_rgb, &Rgb::new(0, 0, 0), &alpha);
-        println!("new lightest: {:?}", new_lightest);
-        let actual_contrast_ratio = contrast_ratio(&new_lightest, &Rgb::new(255, 255, 255));
-        println!("Actual contrast ratio: {}", actual_contrast_ratio);
-    } else {
-        println!("Error opening image image.jpg");
-    }
-
-    netlify_lambda::run(handler(hello)).await?;
+    netlify_lambda::run(handler(respond_with_alpha)).await?;
     Ok(())
 }
 
-async fn hello(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    // Ok(format!(
-    //     "hello {}",
-    //     request
-    //         .query_string_parameters()
-    //         .get("name")
-    //         .unwrap_or_else(|| "stranger")
-    // ))
-    Ok(json!({
-        "name": request.query_string_parameters().get("name").unwrap_or_else(|| "stranger")
-    }))
+async fn respond_with_alpha(request: Request, _: Context) -> Result<impl IntoResponse, Error> {
+    let base64_image = String::from(
+        request
+            .query_string_parameters()
+            .get("base64")
+            .unwrap_or_else(|| ""),
+    );
+    let image = base64_to_image(&base64_image);
+    let resized_image = resize_image(&image);
+    let lightest_rgb = lowest_highest_luminance_rgb(&resized_image).1;
+    println!("lightest background: {:?}", lightest_rgb);
+    let alpha = overlay_opacity(
+        &Rgb::new(255, 255, 255),
+        &lightest_rgb,
+        &Rgb::new(0, 0, 0),
+        4.5,
+    );
+
+    Ok(json!({ "alpha": alpha }))
 }
 
 fn overlay_opacity(
@@ -108,35 +65,6 @@ fn overlay_opacity(
     println!("Delta: {}", delta);
     println!("Overlay alpha: {} {} {}", opacity_r, opacity_g, opacity_b);
     (opacity_r + opacity_g + opacity_b) / 3.0
-}
-
-fn composite_colour(base_colour: &Rgb, overlay_colour: &Rgb, overlay_opacity: &f64) -> Rgb {
-    let r = base_colour.get_red() as f64 * (1.0 - overlay_opacity)
-        + overlay_colour.get_red() as f64 * overlay_opacity;
-    let g = base_colour.get_green() as f64 * (1.0 - overlay_opacity)
-        + overlay_colour.get_green() as f64 * overlay_opacity;
-    let b = base_colour.get_blue() as f64 * (1.0 - overlay_opacity)
-        + overlay_colour.get_blue() as f64 * overlay_opacity;
-    // Rgb::new(r.trunc() as u8, g.trunc() as u8, b.trunc() as u8)
-    Rgb::new(r.ceil() as u8, g.ceil() as u8, b.ceil() as u8)
-}
-
-fn contrast_ratio_from_relative_luminance(
-    relative_luminance_1: &f64,
-    relative_luminance_2: &f64,
-) -> f64 {
-    if relative_luminance_1 < relative_luminance_2 {
-        (relative_luminance_2 + 0.05) / (relative_luminance_1 + 0.05)
-    } else {
-        (relative_luminance_1 + 0.05) / (relative_luminance_2 + 0.05)
-    }
-}
-
-fn contrast_ratio(colour_1: &Rgb, colour_2: &Rgb) -> f64 {
-    contrast_ratio_from_relative_luminance(
-        &relative_luminance(colour_1),
-        &relative_luminance(colour_2),
-    )
 }
 
 // Newton-Raphson solution of delta
@@ -185,22 +113,6 @@ fn delta_from_colour_target_luminance(colour_ratio: &RgbRatio, target_luminance:
     delta_next
 }
 
-fn lowest_highest_luminance(image: &PhotonImage) -> (f64, f64) {
-    let mut highest_luminance = 0.0;
-    let mut lowest_luminance = 1.0;
-    let raw_pixels = image.get_raw_pixels();
-    for pixel in raw_pixels.chunks(4) {
-        let pixel_rgb = Rgb::new(pixel[0], pixel[1], pixel[2]);
-        let pixel_luminance = relative_luminance(&pixel_rgb);
-        if pixel_luminance > highest_luminance {
-            highest_luminance = pixel_luminance;
-        } else if pixel_luminance < lowest_luminance {
-            lowest_luminance = pixel_luminance;
-        }
-    }
-    (lowest_luminance, highest_luminance)
-}
-
 fn lowest_highest_luminance_rgb(image: &PhotonImage) -> (Rgb, Rgb) {
     let mut highest_luminance = 0.0;
     let mut lowest_luminance = 1.0;
@@ -221,28 +133,28 @@ fn lowest_highest_luminance_rgb(image: &PhotonImage) -> (Rgb, Rgb) {
     (lowest_luminance_rgb, highest_luminance_rgb)
 }
 
-fn read_in_colour() -> photon_rs::Rgb {
-    println!("Hex colour: (e.g. \"#ff0044\")");
-    let mut colour_hex = String::new();
-    io::stdin()
-        .read_line(&mut colour_hex)
-        .expect("Sorry, I don't understand, try somthing like '#ff0044'");
-    let colour_hex = colour_hex.trim();
-    let r = match u8::from_str_radix(&colour_hex[1..3], 16) {
-        Ok(num) => num,
-        Err(_) => 0,
-    };
-    let g = match u8::from_str_radix(&colour_hex[3..5], 16) {
-        Ok(num) => num,
-        Err(_) => 0,
-    };
-    let b = match u8::from_str_radix(&colour_hex[5..7], 16) {
-        Ok(num) => num,
-        Err(_) => 0,
-    };
-    println!("{} {} {}", r, g, b);
-    Rgb::new(r, g, b)
-}
+// fn read_in_colour() -> photon_rs::Rgb {
+//     println!("Hex colour: (e.g. \"#ff0044\")");
+//     let mut colour_hex = String::new();
+//     io::stdin()
+//         .read_line(&mut colour_hex)
+//         .expect("Sorry, I don't understand, try somthing like '#ff0044'");
+//     let colour_hex = colour_hex.trim();
+//     let r = match u8::from_str_radix(&colour_hex[1..3], 16) {
+//         Ok(num) => num,
+//         Err(_) => 0,
+//     };
+//     let g = match u8::from_str_radix(&colour_hex[3..5], 16) {
+//         Ok(num) => num,
+//         Err(_) => 0,
+//     };
+//     let b = match u8::from_str_radix(&colour_hex[5..7], 16) {
+//         Ok(num) => num,
+//         Err(_) => 0,
+//     };
+//     println!("{} {} {}", r, g, b);
+//     Rgb::new(r, g, b)
+// }
 
 fn resize_image(image: &PhotonImage) -> PhotonImage {
     let long_side = 256;
@@ -346,46 +258,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_contrast_ratio() {
-        let colour_black = Rgb::new(0, 0, 0);
-        let colour_blue = Rgb::new(0, 0, 255);
-        let colour_white = Rgb::new(255, 255, 255);
-        let colour_yellow = Rgb::new(255, 255, 0);
-
-        assert_eq!(contrast_ratio(&colour_black, &colour_white), 21.0);
-        assert_eq!(contrast_ratio(&colour_white, &colour_black), 21.0);
-        assert_eq!(contrast_ratio(&colour_black, &colour_black), 1.0);
-        assert_eq!(
-            contrast_ratio(&colour_blue, &colour_yellow),
-            8.00163666121113
-        );
-        assert_eq!(
-            contrast_ratio(&colour_yellow, &colour_blue),
-            8.00163666121113
-        );
-    }
-
-    #[test]
-    fn test_contrast_ratio_from_relative_luminance() {
-        assert_eq!(contrast_ratio_from_relative_luminance(&0.0, &1.0), 21.0);
-        assert_eq!(contrast_ratio_from_relative_luminance(&1.0, &0.0), 21.0);
-        assert_eq!(contrast_ratio_from_relative_luminance(&0.0, &0.0), 1.0);
-        assert_eq!(contrast_ratio_from_relative_luminance(&0.5, &0.5), 1.0);
-    }
-
-    #[test]
-    fn test_lowest_highest_luminance() {
-        let input_image = open_image("image.jpg");
-        let result = lowest_highest_luminance(&input_image);
-
-        assert!(result.0 >= 0.0);
-        assert!(result.0 <= 1.0);
-        assert!(result.1 >= 0.0);
-        assert!(result.1 <= 1.0);
-        assert!(result.0 <= result.1);
-    }
-
-    #[test]
     fn test_relative_luminance() {
         let colour_black = Rgb::new(0, 0, 0);
         let colour_blue = Rgb::new(0, 0, 255);
@@ -396,19 +268,6 @@ mod tests {
         assert_eq!(relative_luminance(&colour_blue), 0.0722);
         assert_eq!(relative_luminance(&colour_white), 1.0);
         assert_eq!(relative_luminance(&colour_yellow), 0.9278);
-    }
-
-    #[test]
-    fn test_resize_image() {
-        let input_image = open_image("image.jpg");
-        let output_image = resize_image(&input_image);
-        assert_eq!(output_image.get_width(), 170);
-        assert_eq!(output_image.get_height(), 256);
-
-        let input_image = open_image("image-1.jpg");
-        let output_image = resize_image(&input_image);
-        assert_eq!(output_image.get_width(), 256);
-        assert_eq!(output_image.get_height(), 192);
     }
 
     #[test]
